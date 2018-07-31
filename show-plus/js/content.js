d823cb5dd9e3af9ddd5353aaea9e9368762819c3
xuxiuquan <1312543912@qq.com>
2018-08-01 01:08:30 +0800

    chrome-show-plus, add dom event

--- show-plus/js/content.js
+++ show-plus/js/content.js
@@ -13,12 +13,28 @@ document.addEventListener('DOMContentLoaded', function () {
     document.head.appendChild(temp);
   }();
 });
-
+// https://developer.chrome.com/extensions/messaging
 window.addEventListener('message', function (e) {
   console.log(e);
+  if (e.type === 'message') {
+    return;
+  }
+  
   const type = e.data.type;
-  // 保存数据
-  chrome.storage.sync.set({ color: 'blue' }, function () {
-    console.log('保存成功！');
-  });
+  var key = 'like';
+  if (type === 'share') {
+    key = 'share';
+  } else if (type === 'like') {
+    key = 'like';
+  } else if (type === 'comment') {
+    key = 'comment';
+  }
+  SaveData(key);
 })
+
+
+function SaveData(key) {
+  chrome.storage.sync.get([key], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
+}
--- show-plus/js/inject.js
+++ show-plus/js/inject.js
@@ -1,12 +1,30 @@
+// 延时定时器保证事件可以成功绑定
 setTimeout(() => {
-
-  console.log('this is inject');
-  var x = document.getElementsByClassName('pos');
-  for (let i = 0; i < x.length; i++) {
-    x[i].onmousedown = function () {
-      console.log('sss');
-      window.postMessage({ type: "dddd", info: "ddd" }, '*');
+  console.log('this is inject.js');
+  // 绑定like
+  var like = document.getElementsByClassName('UFILikeLink');
+  console.log(like.length);
+  for (let i = 0; i < like.length; i++) {
+    like[i].onmousedown = function () {
+      console.error('like');
+      window.postMessage({ type: "like", info: "1" }, '*');
     }
   }
-
-}, 1500);
+  // 绑定comment
+  var comment = document.getElementsByClassName('comment_link _5yxe');
+  console.log(comment.length);
+  for (let j = 0; j < comment.length; j++) {
+    comment[j].onmousedown = function () {
+      console.error('error');
+      window.postMessage({ type: "comment", info: "1" }, '*');
+    }
+  }
+  // 绑定share
+  var share = document.querySelectorAll('._42nr > ._1mto:nth-of-type(3)');
+  for (let k = 0; k < share.length; k++) {
+    share[k].onmousedown = function () {
+      console.error('error');
+      window.postMessage({ type: "share", info: "1" }, '*');
+    }
+  }
+}, 15000);
